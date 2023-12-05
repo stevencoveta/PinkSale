303f12e057e1b080377a7103e7b3826b538cb0a0
stevencoveta <shdegroodt@gmail.com>
2023-12-05 19:38:47 +0100

    cms

--- twscrape/api.py
+++ twscrape/api.py
@@ -51,7 +51,7 @@ class API:
 
     # gql helpers
 
-    async def _gql_item(self, op: str, kv: dict, ft: Optional[dict] = None):
+    async def _gql_item(self, op: str, kv: dict, ft: Union[dict, None] = None):
 
 
 
