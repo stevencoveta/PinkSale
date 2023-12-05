1335b7721573adaf370c2ac6b4f9ac6470b0e33f
stevencoveta <shdegroodt@gmail.com>
2023-12-05 20:19:49 +0100

    pinksale wip

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ if st.session_state.is_authenticated:
 
     df = fetch_and_process_data(bucket_name, object_key)
 
-    df = df.drop('Unnamed: 0', axis = 1)
+    #df = df.drop('Unnamed: 0', axis = 1)
 
     df.twitter_last_tweet = df.twitter_last_tweet.astype(str)
     df.to_csv('pinksale.csv')
